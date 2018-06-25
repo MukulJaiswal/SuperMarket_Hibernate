0d6c6d945603fc538bdad4290cdaa1eb931911b3
Mukul Jaiswal <30383198+MukulJaiswal@users.noreply.github.com>
2018-06-25 22:34:16 +0530

    Add files via upload

--- src/main/java/com/supermarket/ConfigureSetting.java
+++ src/main/java/com/supermarket/ConfigureSetting.java
@@ -7,13 +7,22 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.ServiceRegistryBuilder;
 
+/**
+ * This class is the Common SessionFactory class for this Project.
+ * 
+ * @author MukulJaiswal
+ *
+ */
 public class ConfigureSetting {
 
-	  Session database() 
-	  {
+	/**
+	 * Returning the session for Database connectivity.
+	 * @return session
+	 */
+	Session database() {
 		Configuration configuration = new Configuration().configure().addAnnotatedClass(Items.class);
 		ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
-		
+
 		SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
 		Session session = sessionFactory.openSession();
 		return session;
--- src/main/java/com/supermarket/Items.java
+++ src/main/java/com/supermarket/Items.java
@@ -5,6 +5,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+/**
+ * This Class contains data types for Database storage.
+ * @author MukulJaiswal
+ *
+ */
 @Entity
 public class Items {
 
